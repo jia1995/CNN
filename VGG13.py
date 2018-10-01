9619b4d42810d96953e8588437318180e981a88b
json <jia1995@users.noreply.github.com>
2018-10-01 14:04:36 +0800

    Create VGG13

---
+++ VGG13.py
@@ -0,0 +1,87 @@
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras import optimizers
+import numpy as np
+from keras.layers.core import Lambda
+from keras import backend as K
+from keras import regularizers
+
+img_rows,img_cols,img_channels = 32,32,3
+num_classes=10
+def VGG13:
+  # layer1 32*32*3
+  model = Sequential()
+  model.add(Conv2D(64, (3, 3), padding='same',
+                 input_shape=(32, 32, 3),kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.3))
+  # layer2 32*32*64
+  model.add(Conv2D(64, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # layer3 16*16*64
+  model.add(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.4))
+  # layer4 16*16*128
+  model.add(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # layer5 8*8*128
+  model.add(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.4))
+  # layer6 8*8*256
+  model.add(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  # layer7 8*8*256
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # layer8 4*4*256
+  model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.4))
+  # layer9 4*4*512
+  model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  # layer10 4*4*512
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # layer11 2*2*512
+  model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(0.4))
+  # layer12 2*2*512
+  model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  # layer13 2*2*512
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Dropout(0.5))
+  # layer14 1*1*512
+  model.add(Flatten())
+  model.add(Dense(512))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  # layer15 512
+  model.add(Dense(512))
+  model.add(Activation('relu'))
+  model.add(BatchNormalization())
+  # layer16 512
+  model.add(Dropout(0.5))
+  model.add(Dense(10))
+  model.add(Activation('softmax'))
+  # 10
+  model.compile(loss='cosine', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+  model.summary()
+  return model
